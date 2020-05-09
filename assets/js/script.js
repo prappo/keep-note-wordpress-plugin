e64fb06d66f83e0960ce05e05b1fc36b86f61807
Prappo Prince <prappo.prince@gmail.com>
2020-05-09 22:25:50 +0600

    some update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,79 @@
-jQuery(document).ready(function($) { //wrapper
+jQuery(document).ready(function ($) { //wrapper
+
+    /**
+     * ==================================================
+     * Check if the position of the editor is set or not
+     * If not set then set default position left and top
+     * ==================================================
+     */
 
 
+    let default_position_top = 20, default_position_left = 1000
+
+
+    if (localStorage.getItem('kn_editor_post_left')) {
+
+        /**
+         * If position already set previously then this block
+         * will execute :D
+         */
+
+        $('#kn-editor').offset({
+            'left': localStorage.getItem('kn_editor_post_left'),
+            'top': localStorage.getItem('kn_editor_post_top')
+        });
+
+
+    } else {
+
+        /**
+         * If the editor position does not set
+         * Then set the default position
+         */
+
+        $('#kn-editor').offset({
+            'left': default_position_left,
+            'top': default_position_top
+        });
+
+    }
+
+    /**
+     * Show saved note in the textarea filed
+     */
 
     if (localStorage.getItem('kn_txt_data')) {
         $('#kn_txt').val(localStorage.getItem('kn_txt_data'));
     }
 
-    if (localStorage.getItem("kn_editor_visible")) {
+
+    /**
+     * Check and control window content visibility
+     */
+
+    if (localStorage.getItem("kn_editor_visible") == 'yes') {
+
         $('#kn-editor-body').show();
-    } else {
+
+    } else if (localStorage.getItem("kn_editor_visible") == 'no') {
+
         $('#kn-editor-body').hide();
+
+
+    } else {
+
+        $('#kn-editor-body').show();
+
     }
 
 
+    /**
+     * Control draggable window
+     */
+
     $("#kn-editor").draggable({
         handle: '.kn-header',
-        drag: function() {
+        drag: function () {
             var offset = $(this).offset();
             var x = $("#kn-editor").position();
             localStorage.setItem("kn_editor_post_top", x.top);
@@ -24,23 +82,13 @@ jQuery(document).ready(function($) { //wrapper
         }
     });
 
-    $('#kn-editor').offset({
-        'left': localStorage.getItem('kn_editor_post_left'),
-        'top': localStorage.getItem('kn_editor_post_top')
-    });
 
-    $("#kn_txt").on('change keyup paste', function() {
+    $("#kn_txt").on('change keyup paste', function () {
         localStorage.setItem("kn_txt_data", $(this).val());
     });
 
 
-
-
-
-
-
-
-    $('.kn-header__close').click(function() {
+    $('.kn-header__close').click(function () {
 
 
         $('#kn-editor-body').toggle();
@@ -48,18 +96,17 @@ jQuery(document).ready(function($) { //wrapper
 
         if ($("#kn-editor-body").is(':visible')) {
 
-            localStorage.setItem("kn_editor_visible", true);
+            localStorage.setItem("kn_editor_visible", "yes");
 
         } else {
 
-            localStorage.setItem("kn_editor_visible", false);
+            localStorage.setItem("kn_editor_visible", "no");
 
         }
     });
 
 
-
-    $('#kn_btn_save').click(function() {
+    $('#tab_save').click(function () {
         alert('Saved');
     });
 
--- keep-note.php
+++ keep-note.php
@@ -1,8 +1,36 @@
 <?php
 
 /**
- * Plugin Name: Keep Note
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Keep_Note
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Keep Note
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       Simple and light keep note plugin that will help you to save note.
+ * Version:           1.0.0
+ * Author:            Prappo Prince
+ * Author URI:        http://wpsega.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wpsega
+ * Domain Path:       /languages
  */
+
+
+defined('ABSPATH') || exit;
+
+define('KEEP_NOTE_VERSION', 1.00);
+
+
 
 function kn_admin_scripts()
 {
@@ -12,11 +40,14 @@ function kn_admin_scripts()
     wp_enqueue_script('kn_script', plugin_dir_url(__FILE__) . '/assets/js/script.js', array('jquery'), 1.1, true);
     wp_enqueue_style('kn_style', plugin_dir_url(__FILE__) . '/assets/css/style.css', false);
 
+
 }
-add_action('admin_enqueue_scripts', 'kn_admin_scripts');
-add_action('admin_footer', 'kn_html_template');
+
 
 function kn_html_template()
 {
     include 'template.php';
 }
+
+add_action('admin_enqueue_scripts', 'kn_admin_scripts');
+add_action('admin_footer', 'kn_html_template');
